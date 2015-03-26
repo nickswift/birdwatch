f03df17094e163ba2bf2d143e2aaa1d7ab2300a9
Nick Wood <nick.wood@scriptrock.com>
2015-03-25 22:58:52 -0700

    anaconda API crap

--- client/anaconda.go
+++ client/anaconda.go
@@ -4,6 +4,38 @@ import(
 	"github.com/chimeracoder/anaconda"
 )
 
-type AnacondaClient struct {
-	Api *anaconda.TwitterApi
+type Consumer struct {
+	Key    string
+	Secret string
+}
+
+func NewConsumer(key, secret string) *Consumer {
+	return &Consumer{key, secret}
+}
+
+type Access struct {
+	Token  string
+	Secret string
+}
+
+func NewAccess(token, secret string) *Access {
+	return &Access{token, secret}
+}
+
+type TwitterClient struct {
+	Consumer *Consumer
+	Access   *Access
+	Api      *anaconda.TwitterApi
+}
+
+func NewTwitterClient(ckey, ssecret, atoken, asecret string) *TwitterClient {
+	anaconda.SetConsumerKey(ckey)
+	anaconda.SetConsumerSecret(ssecret)
+	api := anaconda.NewTwitterApi(atoken, asecret)
+	
+	return &TwitterClient{
+		Consumer: NewConsumer(ckey, ssecret),
+		Access: NewAccess(atoken, asecret),
+		Api: api
+	}
 }
